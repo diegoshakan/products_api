77003b3d966f3d21da858e96571234fc1d9ba352
Diego Araujo <diego_shakan@hotmail.com>
2022-05-30 20:45:24 -0300

    chore(app): remove errors' message from controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category, optional: true
+
+  validates :name, uniqueness: { case_sensitive: true }
+  validates :description, :price, :quantity, presence: true
 end
