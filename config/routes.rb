df03a2a00fc6997839a6eab83771b8057180332f
Diego Araujo <diego_shakan@hotmail.com>
2022-05-30 20:18:57 -0300

    feat(app): create version to api, change routes and relationship between models

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :categories
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :Api do
+    namespace :V1 do
+      resources :products
+      resources :categories
+    end
+  end
 end
