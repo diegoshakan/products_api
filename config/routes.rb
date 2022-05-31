5e0be80fcd0077dc3937dadf286da6fb89c3d043
Diego Araujo <diego_shakan@hotmail.com>
2022-05-31 08:39:43 -0300

    feat(app): add jwt auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  post '/auth/login', to: 'authentication#login'
+
   namespace :api do
     namespace :v1 do
-      resources :products
       resources :categories
+      resources :products
+      resources :users, param: :_username
     end
   end
 end
