5a84749e20143d723c98e79b0324ee79fa6e737c
Diego Araujo <diego_shakan@hotmail.com>
2022-06-07 20:48:20 -0300

    test(products): modify in validates, tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,5 @@ class Product < ApplicationRecord
   belongs_to :category, optional: true
   belongs_to :user
 
-  validates :name, uniqueness: { case_sensitive: true }
-  validates :description, :price, :quantity, presence: true
+  validates :name, :description, :price, :quantity, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:category).optional(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:quantity) }
+  end
 end
