158ce7f7aa46afcafa0c2f980ed9f4ba1e825757
Diego Araujo <diego_shakan@hotmail.com>
2022-06-11 11:21:04 -0300

    feat(category): modify fields and relationship

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe 'associations' do
-    it { should belong_to(:user) }
     it { should have_many(:products).dependent(:destroy) }
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:label) }
   end
 end
