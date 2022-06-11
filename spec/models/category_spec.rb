a54c6903d8b83201cfe3aba5242c1ce2982d8767
Diego Araujo <diego_shakan@hotmail.com>
2022-06-11 11:50:39 -0300

    test(app): fixed tests models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Category, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:label) }
   end
+
+  describe 'enums' do
+    it { should define_enum_for(:situation).with_values(active: "active", inactive: "inactive")
+                                           .backed_by_column_of_type(:string) }
+  end
 end
--- spec/models/type_user_spec.rb
+++ spec/models/type_user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe TypeUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:users) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:label) }
+  end
+
+  describe 'enums' do
+    it { should define_enum_for(:situation).with_values(active: "active", inactive: "inactive")
+                                           .backed_by_column_of_type(:string) }
+  end
 end
